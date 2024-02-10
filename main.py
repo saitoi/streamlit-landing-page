b2e3ee54ac26384fc386487e6ba7d83f343ef361
saitoi <pedrohhs@dcc.ufrj.br>
2024-02-09 23:42:04 -0300

    Slight background mods

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ with st.sidebar:
     st.write('This is not right')
 
 # TITLE IMAGE
-st.image(image='assets/saitos-lair.png', width=750)
+st.image(image='assets/saitos-lair-shrek.png', width=750)
 
 st_elements.divider_element()
 
 # COLUMNS
-col1, col2 = st.columns([0.7, 0.3], gap='small')
+col1, col2 = st.columns([0.6, 0.4], gap='small')
 
 with col1:
     st.markdown("""
@@ -28,39 +28,43 @@ with col1:
     """, unsafe_allow_html=True)
 
 with col2:
-    st.markdown("""
-    <style>
-    .circle-image {
-        width: 250px;
-        height: 250px;
-        border-radius: 50%;
-        overflow: hidden;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-        background: linear-gradient(to top, white, gray, black);
-        background-clip: padding-box; /* Ensures the background (gradient) is only visible in the padding area */
-        border: 5px solid transparent;
-        transition: transform 0.5s ease;
-    }
-
-    .circle-image img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    
-    .circle-image:hover {
-        transform: rotate(180deg);
-        cursor: pointer;
-    }
-    </style>
-    
-    <div class="circle-image">
-        <img src="https://iili.io/J1M8Mog.png" alt="My Rectangular Image">
-    </div>
-    """, unsafe_allow_html=True)
-
+    # st.markdown("""
+    # <style>
+    # .circle-image {
+    #     width: 250px;
+    #     height: 250px;
+    #     border-radius: 50%;
+    #     overflow: hidden;
+    #     box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+    #     background: linear-gradient(to top, white, gray, black);
+    #     background-clip: padding-box; /* Ensures the background (gradient) is only visible in the padding area */
+    #     border: 5px solid transparent;
+    #     transition: transform 0.5s ease;
+    # }
+    #
+    # .circle-image img {
+    #     width: 100%;
+    #     height: 100%;
+    #     object-fit: cover;
+    # }
+    #
+    # .circle-image:hover {
+    #     transform: rotate(180deg);
+    #     cursor: pointer;
+    # }
+    # </style>
+    #
+    # <div class="circle-image">
+    #     <img src="https://iili.io/J1M8Mog.png" alt="My Rectangular Image">
+    # </div>
+    # """, unsafe_allow_html=True)
+    st.image(image='assets/playdough-nerd.png', use_column_width=True)
     st.caption("""
-    <br>
+    <style>
+        center {
+            color: black;
+        }
+    </style>
     <center>This is what I look like</center>
     """, unsafe_allow_html=True)
 
